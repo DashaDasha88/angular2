b02a9cac61f234d39afa38b0eb90bb0c4c657d60
Dasha <dasha.kotova.mail@gmail.com>
2021-10-04 18:09:24 -0400

    modify task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,7 @@ export class TaskService {
 
   private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
